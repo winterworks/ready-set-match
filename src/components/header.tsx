d910da72775f87d3e1f64ef67f27c1eef3e12c14
winterworks <mail@bramwinter.nl>
2024-08-13 20:33:46 +0200

    Add the first version of the ready set match icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { Divider } from '@mui/material';
+import { Divider, Grid } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
 
 export function Header() {
   return (
     <>
-      <Link to="/" >
-        <Typography component="h1" variant="h2" align="center" gutterBottom>
-          Ready Set Match
-        </Typography>
-      </Link>
+      <Grid container item justifyContent={"center"} columnGap={2} >
+        <Grid item alignContent={"center"}>
+          <Link to="/" >
+            <img src="/images/icon.svg" width="50px" />
+          </Link>
+        </Grid>
+        <Grid  alignContent={"center"}>
+          <Link to="/" >
+            <Typography component="h1" variant="h2" alignContent={"center"}>
+              Ready Set Match
+            </Typography>
+          </Link>
+        </Grid>
+      </Grid>
       <Divider sx={{ width: "100%", marginBottom: 4 }}/>
     </>
   )
