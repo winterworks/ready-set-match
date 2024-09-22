eedea5a7864964f8b2a7b07d8c4224a3b6c24282
winterworks <mail@bramwinter.nl>
2024-09-22 07:32:41 +0200

    Remove specific font and use the default instead

--- src/theme.tsx
+++ src/theme.tsx
@@ -2,10 +2,6 @@ import { createTheme, responsiveFontSizes } from '@mui/material/styles'
 import { red } from '@mui/material/colors'
 
 import './global.css'
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
 
 // A custom theme for this app
 const theme = createTheme({
